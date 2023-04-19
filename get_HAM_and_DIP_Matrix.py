c39d9ccf28de3f3a533abf14b91336200d1d7ede
bradenmweight <braden.m.weight@gmail.com>
2023-04-19 13:04:35 -0400

    FIXED BUG: QCHEM NUCLEAR DIPOLES...

--- get_HAM_and_DIP_Matrix.py
+++ get_HAM_and_DIP_Matrix.py
@@ -47,8 +47,149 @@ def plot_energies(E_TDA,E_RPA):
     plt.savefig(f"{DATA_DIR}/E_TDA_RPA_DIFF.jpg", dpi=600)
     plt.clf()
 
+def get_atoms():
 
+    with open("QCHEM.out","r") as file01:
+        lines       = file01.readlines()
+        atomFLAG    = False
+        atom_coords = []
+        atom_labels = []
+        units       = ""
+        for count, line in enumerate(lines):
+            t = line.split()
+            if ( len(t) > 1 and t[0] == "Standard" and t[1] == "Nuclear" ):
+                units = t[-1].split("(")[1].split(")")[0]
+                atomFLAG = True
+                counter = 3
+                while (atomFLAG == True):
+                    S = lines[count+counter].split()
+                    if ( len(S) == 5 ):
+                        atom_labels.append( S[1] )
+                        atom_coords.append( S[2:] )
+                    if ( "----------------------------------------------------------------" in S ):
+                        break
+                    counter += 1
 
+    return atom_labels, np.array(atom_coords).astype(float), units.upper()
+
+def get_nuclear_charges():
+    dict = {
+"H":0,\
+"He":0,\
+"Li":0,\
+"Be":0,\
+"B":0,\
+"C":0,\
+"N":0,\
+"O":0,\
+"F":0,\
+"Ne":0,\
+"Na":0,\
+"Mg":0,\
+"Al":0,\
+"Si":0,\
+"P":0,\
+"S":0,\
+"Cl":0,\
+"Ar":0,\
+"K":0,\
+"Ca":0,\
+"Sc":0,\
+"Ti":0,\
+"V":0,\
+"Cr":0,\
+"Mn":0,\
+"Fe":0,\
+"Co":0,\
+"Ni":0,\
+"Cu":0,\
+"Zn":0,\
+"Ga":0,\
+"Ge":0,\
+"As":0,\
+"Se":0,\
+"Br":0,\
+"Kr":0,\
+"Rb":0,\
+"Sr":0,\
+"Y":0,\
+"Zr":0,\
+"Nb":0,\
+"Mo":0,\
+"Tc":0,\
+"Ru":0,\
+"Rh":0,\
+"Pd":0,\
+"Ag":0,\
+"Cd":0,\
+"In":0,\
+"Sn":0,\
+"Sb":0,\
+"Te":0,\
+"I":0,\
+"Xe":0,\
+"Cs":0,\
+"Ba":0,\
+"La":0,\
+"Ce":0,\
+"Pr":0,\
+"Nd":0,\
+"Pm":0,\
+"Sm":0,\
+"Eu":0,\
+"Gd":0,\
+"Tb":0,\
+"Dy":0,\
+"Ho":0,\
+"Er":0,\
+"Tm":0,\
+"Yb":0,\
+"Lu":0,\
+"Hf":0,\
+"Ta":0,\
+"W":0,\
+"Re":0,\
+"Os":0,\
+"Ir":0,\
+"Pt":0,\
+"Au":0,\
+"Hg":0,\
+"Tl":0,\
+"Pb":0,\
+"Bi":0,\
+"Po":0,\
+"At":0,\
+"Rn":0,}
+    for count,at in enumerate(dict):
+        dict[at] = float(count+1)
+
+    return dict
+
+def get_nuclear_dipole():
+    # TODO -- Shift to center of charge before computing dipole for charges systems
+    
+    ANG_to_DEBYE = 4.80320
+    DEBYE_to_AU  = 1/2.54158
+    
+    atom_labels, atom_coords, units = get_atoms()
+    nuclear_charge_dict = get_nuclear_charges()
+    MU_NUC = np.zeros((3))
+    for at, atom in enumerate( atom_labels ):
+        MU_NUC[0] += nuclear_charge_dict[atom] * atom_coords[at,0]
+        MU_NUC[1] += nuclear_charge_dict[atom] * atom_coords[at,1]
+        MU_NUC[2] += nuclear_charge_dict[atom] * atom_coords[at,2]
+    if ( units == "Angstroms".upper() ):
+        MU_NUC *= ANG_to_DEBYE * DEBYE_to_AU
+    elif ( units == "Bohrs".upper() ):
+        print("Only works for Angstrom*|e| for now.")
+        exit()
+    else:
+        print("Only works for Angstrom*|e| for now.")
+        exit()
+    
+    print("Nuclear Dipole:", MU_NUC)
+
+    return MU_NUC
 
 
 sp.call(f"mkdir -p {DATA_DIR}", shell=True)
@@ -74,8 +215,9 @@ else:
 sp.call(f"rm {DATA_DIR}/EXC_ENERGIES.dat",shell=True)
 
 ##### Create arrays to store all data #####
-EAD = np.zeros(( NROOTS+1 )) # Add ground state
-DIP = np.zeros(( NROOTS+1, NROOTS+1, 3 )) # Add ground state
+EAD     = np.zeros(( NROOTS+1 )) # Add ground state
+DIP     = np.zeros(( NROOTS+1, NROOTS+1, 3 )) # Add ground state
+MU_NUC  = get_nuclear_dipole()
 ###########################################
 EAD[0] = GS_ENERGY # Already in a.u.
 if ( RPA == True ):
@@ -93,7 +235,7 @@ else:
 
 ##### Look for dipoles from TDA #####
 DIP[0,0,:] = np.array( sp.check_output("grep -A 4 'Electron Dipole Moments of Ground State' QCHEM.out | tail -n 1", shell=True).decode().split()[1:] ).astype(float)
-DIP[np.arange(1,NROOTS+1), np.arange(1,NROOTS+1),:] = np.array( sp.check_output(f"grep -m 1 -A {3+NROOTS} 'Electron Dipole Moments of Singlet Excited State' QCHEM.out | tail -n {NROOTS}", shell=True).decode().split() ).astype(float).reshape(NROOTS,4)[:,1:]
+DIP[np.arange(1,NROOTS+1), np.arange(1,NROOTS+1),:]  = np.array( sp.check_output(f"grep -m 1 -A {3+NROOTS} 'Electron Dipole Moments of Singlet Excited State' QCHEM.out | tail -n {NROOTS}", shell=True).decode().split() ).astype(float).reshape(NROOTS,4)[:,1:]
 DIP[0, np.arange(1,NROOTS+1),:] = np.array( sp.check_output(f"grep -m 1 -A {3+NROOTS} 'Transition Moments Between Ground and Singlet Excited States' QCHEM.out | tail -n {NROOTS}", shell=True).decode().split() ).astype(float).reshape(NROOTS,6)[:,2:5]
 DIP[np.arange(1,NROOTS+1),0,:] = np.array( sp.check_output(f"grep -m 1 -A {3+NROOTS} 'Transition Moments Between Ground and Singlet Excited States' QCHEM.out | tail -n {NROOTS}", shell=True).decode().split() ).astype(float).reshape(NROOTS,6)[:,2:5]
 
@@ -103,6 +245,10 @@ tmp = np.array( sp.check_output(f"grep -m 1 -A {3+NTERMS} 'Transition Moments Be
 for J,K,dx,dy,dz in tmp:
     DIP[int(J),int(K),:] = np.array([dx,dy,dz])
     DIP[int(K),int(J),:] = np.array([dx,dy,dz])
+
+# Add nuclear dipole (with negative sign) from perm. dipoles:
+for state in range(NROOTS+1):
+    DIP[state,state,:] -= MU_NUC
 
 np.save(f"{DATA_DIR}/DIPOLE_TDA.dat.npy", DIP)
 plot_dipole(DIP,"TDA")
@@ -124,8 +270,11 @@ if ( RPA == True ):
         DIP[int(J),int(K),:] = np.array([dx,dy,dz])
         DIP[int(K),int(J),:] = np.array([dx,dy,dz])
 
+    # Add nuclear dipole (with negative sign) from perm. dipoles:
+    for state in range(NROOTS+1):
+        DIP[state,state,:] -= MU_NUC
+    
     np.save(f"{DATA_DIR}/DIPOLE_RPA.dat.npy", DIP)
-
     plot_dipole(DIP,"RPA")
 
 
