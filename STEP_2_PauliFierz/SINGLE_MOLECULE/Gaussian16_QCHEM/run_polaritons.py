1bc40b0bf2b7b6463af6994aa88fc2e317e01c0b
bradenmweight <braden.m.weight@gmail.com>
2025-02-09 16:48:40 -0500

    Updated windows version

--- STEP_2_PauliFierz/SINGLE_MOLECULE/Gaussian16_QCHEM/run_polaritons.py
+++ STEP_2_PauliFierz/SINGLE_MOLECULE/Gaussian16_QCHEM/run_polaritons.py
@@ -1,18 +1,18 @@
 import numpy as np
 import os
 
-WC   = 3.154 # eV
+WC   = 3.00 # eV
 EPOL = "111" # Cavity polarization -- "111", "001", "110", "101", "011"
 NM   = 5 # Number of electronic states (including ground state)
 NF   = 5 # Number of Fock states
 
 A0MIN = 0.0
-A0MAX = 0.5
-dA0   = 0.1
+A0MAX = 0.2
+dA0   = 0.005
 
 A0_LIST = np.arange( A0MIN, A0MAX+dA0, dA0 )
 
 for A0 in A0_LIST:
-    os.system(f"python3 PF.py {NM} {NF} {A0} {WC} {EPOL}")
+    os.system(f"python3 Pauli-Fierz_DdotE.py {NM} {NF} {A0} {WC} {EPOL}")
 
 
